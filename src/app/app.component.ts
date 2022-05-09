e076db8034541bbe510d86745dc4b843ff799da6
Michal <michalgrzegorczyk04@gmail.com>
2022-05-09 14:32:05 +0200

    Add first animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        'transform': 'translateX(0)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        'transform': 'translateX(100px)'
+      })),
+      transition('normal => highlighted', animate(1000)),
+      transition('highlighted => normal', animate(2000))
+    ])
+  ]
 })
 export class AppComponent {
+  state = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
-    onAdd(item) {
-      this.list.push(item);
-    }
+  onAnimate() {
+    this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+  }
+
+  onAdd(item) {
+    this.list.push(item);
+  }
 }
